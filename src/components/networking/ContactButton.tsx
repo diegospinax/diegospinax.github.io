4d319ee328808de4ba40e6140fdad0cf54569ae5
Diego Ospina <oadiexiii@gmail.com>
2025-11-18 17:50:25 -0500

    refactor contact button component

--- src/components/networking/ContactButton.tsx
+++ src/components/networking/ContactButton.tsx
@@ -10,7 +10,7 @@ export const ContactButton = ({ children, link }: Props) => {
         <a
             href={link}
             target='_blank'
-            className="flex gap-2.5 px-3 py-1.5 items-center bg-charcoal-blue rounded w-full sm:max-w-max text-white-smoke">
+            className="flex gap-2.5 px-3 py-1.5 items-center bg-white/5 border border-charcoal-blue/20 rounded-lg w-full sm:max-w-max text-gray-300 hover:border-lilac-ash/50 hover:text-lilac-ash transition-all duration-300">
             {children}
         </a>
     )
